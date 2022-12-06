0362f62934d20dd9b27b88eeee0631bc8b51ad65
hqdem <ilyadem4@gmail.com>
2022-12-06 18:10:00 +0300

    remove unused imports

--- quiz_api/viewsets.py
+++ quiz_api/viewsets.py
@@ -1,11 +1,8 @@
-from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
-from rest_framework.response import Response
 
 from .models import Topic, Test, Question, Option
 from .serializers import (
     TopicSerializer,
-    TestSerializer,
     QuestionSerializer,
     OptionSerializer,
     TestAndCreateSerializer,
